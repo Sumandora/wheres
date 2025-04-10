1cef6fb42b645b9d5524e94a6eab384bd663ee68
johannes <johannesmiesenhardt@gmail.com>
2025-04-10 12:02:57 +0200

    Add check for Python 3.13

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+which python3.13 >/dev/null 2>&1 || { echo "Python 3.13 is required"; exit; }
+
 WHERES=$(dirname "$0")
 
 rm -rf $WHERES/venv
 
-python -m venv $WHERES/venv
+python3.13 -m venv $WHERES/venv
 . $WHERES/venv/bin/activate
 pip install -r $WHERES/requirements.txt
