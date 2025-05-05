f94b1103df78ea5c3aab41cc6f8195b03b0018c7
johannes <johannesmiesenhardt@gmail.com>
2025-05-05 23:09:37 +0200

    Satisfy shellcheck

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@ which python3.13 >/dev/null 2>&1 || { echo "Python 3.13 is required"; exit; }
 
 WHERES=$(dirname "$0")
 
-rm -rf $WHERES/venv
+rm -rf "$WHERES/venv"
 
-python3.13 -m venv $WHERES/venv
-. $WHERES/venv/bin/activate
-pip install -r $WHERES/requirements.txt
+python3.13 -m venv "$WHERES/venv"
+# shellcheck source=/dev/null
+. "$WHERES/venv/bin/activate"
+pip install -r "$WHERES/requirements.txt"
